0632300e4289566a0e139971fabadeae4be8dc42
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-04-07 15:35:44 +0900

    fix: 降順

--- src/components/schedule/ScheduleList.tsx
+++ src/components/schedule/ScheduleList.tsx
@@ -1,6 +1,7 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import { Schedule } from "./Schedule";
+import { Text } from "@chakra-ui/react";
 
 interface ScheduleListProps {
   width?: string | number;
@@ -10,10 +11,11 @@ interface ScheduleListProps {
 export const ScheduleList = ({ width, schedules }: ScheduleListProps) => {
   // 日付降順にソートする関数
   const sortSchedulesByDate = (schedules: Schedule[]) => {
-    return schedules.slice().sort((a, b) => b.date - a.date);
+    return schedules.slice().sort((a, b) => a.date - b.date);
   }
 
   // ソートしたschedulesを使う
+
   const sortedSchedules = sortSchedulesByDate(schedules);
 
   const boxHeight = "100px";
@@ -21,8 +23,8 @@ export const ScheduleList = ({ width, schedules }: ScheduleListProps) => {
     <SimpleGrid columns={1} spacing={2} overflow="auto" width={width} maxH="600">
       {sortedSchedules.map(schedule => (
         <Box key={schedule.id} bg="red" height={boxHeight}>
-          {/* ここでscheduleの内容を表示 */}
-          {schedule.id}
+          <Text>{schedule.date}</Text>
+          <Text>{schedule.description}</Text>
         </Box>
       ))}
     </SimpleGrid>
