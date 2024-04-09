9f4fc0c55a8caea4bf4779254389c4fc5fc4177a
harsh <66548698+harsssh@users.noreply.github.com>
2024-04-10 03:49:52 +0900

    rm old code

--- src/backend/application/schedule/query_service/getTentativeScheduleList.ts
+++
@@ -1,5 +0,0 @@
-import { Schedule } from '@/backend/domain/model/old/schedule/schedule'
-
-export interface IGetTentativeScheduleListQueryService {
-  execute(userId: string): Promise<Schedule[]>
-}
--- src/backend/application/schedule/query_service/getUserSchedulesWithinPeriod.ts
+++
@@ -1,5 +0,0 @@
-import { Schedule } from '@/backend/domain/model/old/schedule/schedule'
-
-export interface IGetUserSchedulesWithinPeriodQueryService {
-  execute(userId: string, startDate: Date, endDate: Date): Promise<Schedule[]>
-}
