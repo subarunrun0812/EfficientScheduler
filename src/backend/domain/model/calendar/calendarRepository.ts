36fdc2531db3aec717ed491960babbbb727413a8
kaaaaakun_ <you@example.com>
2024-04-12 17:46:16 +0900

    feat : CalendarRepositoryの実装

--- src/backend/domain/model/calendar/calendarRepository.ts
+++ src/backend/domain/model/calendar/calendarRepository.ts
@@ -1,8 +1,9 @@
+import { Event } from '../event/event'
 import { Calendar } from './calendar'
 
 export interface ICalendarRepository {
   save(calendar: Calendar): Promise<void>
-  find(id: string): Promise<Calendar | null>
-  findByUserId(userId: string): Promise<Calendar | null>
-  delete(calendar: Calendar): Promise<void>
+  find(id: string): Promise<Event | null>
+  findByUserId(userId: string): Promise<Event[] | null>
+  delete(eventId: string): Promise<void>
 }
