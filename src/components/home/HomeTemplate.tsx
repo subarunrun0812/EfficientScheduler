eeba017a0e413b95b65cf6cd94a70ae5802c6604
harsh <66548698+harsssh@users.noreply.github.com>
2024-04-14 10:45:35 +0900

    home

--- src/components/home/HomeTemplate.tsx
+++ src/components/home/HomeTemplate.tsx
@@ -2,67 +2,54 @@
 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { Box, Flex, HStack, VStack, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Flex, useBreakpointValue, VStack } from '@chakra-ui/react'
 import { ScheduleList } from './ScheduleList'
 import { Schedule } from '../type/type'
 import { CreateScheduleButton } from './CreateScheduleButton'
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
+import { Event } from '@/backend/domain/model/event/event'
 
 export const HomeTemplate = () => {
+  const [tentativeEvents, setTentativeEvents] = useState<Schedule[]>([])
   const breakpoint = useBreakpointValue(
     { base: 'base', md: false },
     { ssr: true },
   )
 
-  const scheduleList: Schedule[] = [
-    {
-      id: 1,
-      title: 'Meeting',
-      date: 20230931,
-      description: 'Meeting with the team',
-    },
-    {
-      id: 2,
-      title: 'Interview',
-      date: 20230930,
-      description: 'Interview with the candidate',
-    },
-    {
-      id: 3,
-      title: 'Lunch',
-      date: 20230903,
-      description: 'Lunch with the team',
-    },
-    {
-      id: 4,
-      title: 'Presentation',
-      date: 20230904,
-      description: 'Presenting to clients',
-    },
-    {
-      id: 5,
-      title: 'Training',
-      date: 20230905,
-      description: 'Training session for new employees',
-    },
-    {
-      id: 6,
-      title: 'Conference',
-      date: 20230906,
-      description: 'Attending a conference',
-    },
-    {
-      id: 7,
-      title: 'Project Deadline',
-      date: 20230907,
-      description: 'Finalizing project deliverables',
-    },
-  ]
+  const fetchTentativeEvents = async () => {
+    const supabase = createClient()
+    const { data, error } = await supabase.auth.getUser()
+    if (error) {
+			throw new Error('Failed to fetch user')
+    }
+
+    const userId = data.user.id
+    const response = await fetch(`/api/tentativeEvents?user_id=${userId}`)
+    const json = await response.json()
+    console.log(json)
+    return json as Event[] // TODO: validation
+  }
+
+  useEffect(() => {
+    fetchTentativeEvents().then(events => {
+      setTentativeEvents(events.map((e): Schedule => {
+				return {
+					id: e.id,
+					title: e.title,
+					date: 0, // TODO
+					description: "hoge" // TODO
+				}
+			}))
+    })
+  }, [])
+
   return breakpoint ? (
     // mobile view
     <VStack gap={4}>
       <VStack padding={4} gap={4} width='100%'>
         <CreateScheduleButton width='100%' height='70px' />
-        <ScheduleList width='100%' schedules={scheduleList} />
+        <ScheduleList width='100%' schedules={tentativeEvents} />
       </VStack>
       <Box width='100%' padding={10}>
         <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' />
@@ -73,7 +60,7 @@ export const HomeTemplate = () => {
       <Flex gap={5}>
         <VStack width='30%'>
           <CreateScheduleButton width='70%' height='90px' />
-          <ScheduleList width='100%' schedules={scheduleList} />
+          <ScheduleList width='100%' schedules={tentativeEvents} />
         </VStack>
         <Box width='70%'>
           <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' />
